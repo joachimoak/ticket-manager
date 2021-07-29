d5c3a8627d1d752256d47538a47e9a798a1ac600
joachimoak <koami.kpognon@yahoo.com>
2021-07-29 23:25:41 +0200

    chore: implemented server starting

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import dotenv from "dotenv";
-import { User } from "./entity/user";
+import express from "express";
+// common
+import morgan from "morgan";
+import cors from "cors";
+import bodyParser from "body-parser";
+import compression from "body-parser";
+////
+
 
 dotenv.config();
 
@@ -32,9 +39,55 @@ createConnection({
         // await userRepository.save(user);
 
         // console.log("User has been saved");
+
+        const app = express();
+
+        // handle body request parsing
+        app.use(
+            bodyParser.json({
+                limit: "10mb"
+            })
+        );
+        app.use(
+            bodyParser.urlencoded({
+                limit: "10mb",
+                extended: true,
+                parameterLimit: 1000000,
+            })
+        );
+
+        // handle cors
+        app.use(cors());
+
+        //configure morgan ends here
+        app.use(morgan("dev")); // configure morgan
+
+        // handle compression
+        app.use(compression());
+
+        // Routing
+
+
+        const _app_folder = "www";
+
+        app.all("*", (req, res) => {
+            res.status(200).sendFile(`/`, {
+                root: _app_folder,
+            });
+        });
+
+        ///// Start server
+        app.listen(process.env.SERVICE_PORT, () => {
+            console.log("ENV = " + process.env.ENVIRONMENT);
+            console.log("Express server launched !");
+            console.log("Server Started. Listening on " + process.env.SERVICE_PORT);
+            console.log("Node version in use: " + process.version);
+        });
+
     } catch (e) {
         console.error(e);
     }
 
 
 }).catch(error => console.log(error));
+
