cfe4acc13bac112154d6496471cd75c5af4f0c50
joachimoak <koami.kpognon@yahoo.com>
2021-07-29 18:13:10 +0200

    chore: switch to typescript...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,20 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {},
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 };
