30a67ac2157dee745a58545bbe94a3da29ccafee
joachimoak <koami.kpognon@yahoo.com>
2021-07-29 17:07:13 +0200

    chore: mandatory: install package reflect-metadata

--- src/server.js
+++
@@ -1,9 +0,0 @@
-(function example() {
-  const a = 1;
-  const b = a;
-  const c = a;
-
-  console.log(a); // throws ReferenceError
-  console.log(b); // throws ReferenceError
-  console.log(c);
-})();
