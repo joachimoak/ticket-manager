db23053fc6640ad37e7648baee5e14f9af49775e
joachimoak <koami.kpognon@yahoo.com>
2021-07-30 19:43:15 +0200

    chore: comment table creation migration

--- src/entity/comment.ts
+++ src/entity/comment.ts
@@ -1,24 +1,24 @@
-// import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 // import { Ticket } from "./ticket";
 // import { User } from "./user"
 
-// @Entity({ name: "comment" })
-// export class Comment {
-//     @PrimaryGeneratedColumn()
-//     id!: number;
+@Entity({ name: "comment" })
+export class Comment {
+    @PrimaryGeneratedColumn()
+    id!: number;
 
-//     @Column()
-//     content!: string;
+    @Column()
+    content!: string;
 
-//     @Column({ default: () => "NOW()" })
-//     createDate!: Date;
+    @Column({ default: () => "NOW()" })
+    createDate!: Date;
 
-//     @Column({ default: () => "NOW()" })
-//     updateDate!: Date;
+    @Column({ default: () => "NOW()" })
+    updateDate!: Date;
 
-//     @ManyToOne(() => Ticket, ticket => ticket.comments, { onDelete: 'CASCADE' })
-//     ticket!: Ticket;
+    // @ManyToOne(() => Ticket, ticket => ticket.comments, { onDelete: 'CASCADE' })
+    // ticket!: Ticket;
 
-//     @ManyToOne(() => User, user => user.comments) // opted to keep deleted users comments
-//     user!: User;
-// }
+    // @ManyToOne(() => User, user => user.comments) // opted to keep deleted users comments
+    // user!: User;
+}
